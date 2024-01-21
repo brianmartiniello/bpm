9e96868e3e8f099aead9d1f15d0fb47966a65727
Brian Martiniello <bmartiniello@gmail.com>
2024-01-21 18:51:04 -0500

    develop: Initial cmake set up

---
+++ code/core/include/bpm/core/Version.hxx
@@ -0,0 +1,16 @@
+// HXX_HEADER
+
+#ifndef BPM_CORE_VERSION_HXX
+#define BPM_CORE_VERSION_HXX
+
+#include<string>
+
+namespace bpm
+{
+   namespace core
+   {
+      std::string version();   
+   }
+}
+
+#endif
---
+++ code/core/src/Version.cxx
@@ -0,0 +1,16 @@
+// CXX_HEADER
+
+
+#include<bpm/core/Version.hxx>
+
+
+namespace bpm
+{
+   namespace core
+   {
+      std::string version()
+      {
+         return "HELLO WORLD!";
+      }
+   }
+}
