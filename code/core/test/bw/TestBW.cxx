730c750dc33e3dfe1c5ca8ff776e93aca254e237
Brian Martiniello <bmartiniello@gmail.com>
2025-12-17 19:25:51 -0500

    develop: Update work function

--- code/core/test/bw/TestBW.cxx
+++ code/core/test/bw/TestBW.cxx
@@ -1,26 +1,35 @@
-#include <iostream>
-#include <vector>
-#include <thread>
+
 #include <chrono>
 #include <cmath>
+#include <iostream>
 #include <string>
+#include <thread>
+#include <vector>
 
-typedef double DataType;
-
-void work_func(DataType* data, size_t start, size_t end, bool compute_heavy) {
-    if (!compute_heavy) {
-        for (size_t i = start; i < end; ++i) {
-            data[i] = data[i] * 0.5 + 0.2;
-        }
-    } else {
-        for (size_t i = start; i < end; ++i) {
-            DataType val = data[i];
-            for (int j = 0; j < 100; ++j) {
-                val = std::sqrt(std::sin(val) + 2.0);
-            }
-            data[i] = val;
-        }
-    }
+void work(double* data,
+          std::size_t start,
+          std::size_t end,
+          bool computeHeavy)
+{
+   if (true == computeHeavy)
+   {
+      for (auto i = start; i < end; ++i)
+      {
+         auto val = data[i];
+         for (auto j = 0; j < 100; ++j)
+         {
+            val = std::sqrt(std::sin(val) + 2.0);
+         }
+         data[i] = val;
+      }
+   }
+   else
+   {
+      for (auto i = start; i < end; ++i)
+      {
+         data[i] = data[i] * 0.5 + 0.2;
+      }
+   }
 }
 
 int main(int argc, char** argv) {
@@ -34,7 +43,7 @@ int main(int argc, char** argv) {
     size_t N = std::stoull(argv[3]);
     bool compute_heavy = (mode == "compute");
 
-    std::vector<DataType> data(N);
+    std::vector<double> data(N);
     std::vector<std::thread> threads;
     size_t chunk_size = N / num_threads;
 
@@ -54,7 +63,7 @@ int main(int argc, char** argv) {
     for (int i = 0; i < num_threads; ++i) {
         size_t start = i * chunk_size;
         size_t end = (i == num_threads - 1) ? N : (i + 1) * chunk_size;
-        threads.emplace_back(work_func, data.data(), start, end, compute_heavy);
+        threads.emplace_back(work, data.data(), start, end, compute_heavy);
     }
     for (auto& t : threads) t.join();
     auto end_time = std::chrono::high_resolution_clock::now();
