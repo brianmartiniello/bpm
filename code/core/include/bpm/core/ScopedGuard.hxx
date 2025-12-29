456bfe8a1d3ac526308343cb350333a2a41acda8
Brian Martiniello <bmartiniello@gmail.com>
2025-12-28 20:27:34 -0500

    develop: Add ScopedGuard

---
+++ code/core/include/bpm/core/ScopedGuard.hxx
@@ -0,0 +1,47 @@
+
+#ifndef BPM_CORE_SCOPED_GUARD_HXX
+#define BPM_CORE_SCOPED_GUARD_HXX
+
+namespace bpm
+{
+   namespace core
+   {
+      template <typename F>
+      class ScopeGuard
+      {
+         public:
+         
+            explicit ScopeGuard(F&& f)
+               : cleanup_(std::forward<F>(f))
+               , active_(true)
+            {
+            }
+    
+            ~ScopeGuard()
+            {
+               if (active_) cleanup_();
+            }
+
+            ScopeGuard(const ScopeGuard&) = delete;
+         
+            ScopeGuard& operator=(const ScopeGuard&) = delete;
+    
+            ScopeGuard(ScopeGuard&&) = delete;
+            
+            ScopeGuard& operator=(ScopeGuard&&) = delete;
+
+            void dismiss()
+            {
+               active_ = false;
+            }
+
+         private:
+    
+            F cleanup_;
+            bool active_;
+
+      };
+   }
+}
+
+#endif
---
+++ code/core/test/ut/TestScopedGuard.cxx
@@ -0,0 +1,62 @@
+
+
+#include <cstdint>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <bpm/core/Logger.hxx>
+#include <bpm/core/ScopedGuard.hxx>
+
+
+class TestScopedGuard : public ::testing::Test
+{
+   public:
+
+      TestScopedGuard() = default;
+
+      void test()
+      {
+         {
+            BPM_SCOPED_TRACE_COUT("Active");
+
+            auto count = 0U;
+
+            {
+               bpm::core::ScopeGuard scopeGuard([&](){ ++count; });
+               (void) scopeGuard;
+            }
+
+            EXPECT_EQ(count, 1);
+         }
+         
+         {
+            BPM_SCOPED_TRACE_COUT("Dismiss");
+
+            auto count = 0U;
+
+            {
+               bpm::core::ScopeGuard scopeGuard([&](){ ++count; });
+               scopeGuard.dismiss();
+            }
+
+            EXPECT_EQ(count, 0);
+         }
+      }
+
+   private:
+
+};
+
+
+TEST_F(TestScopedGuard, test)
+{
+   test();
+}
+
+
+int main(int argc, char** argv)
+{
+   ::testing::InitGoogleTest(&argc, argv);
+   return RUN_ALL_TESTS();
+}
