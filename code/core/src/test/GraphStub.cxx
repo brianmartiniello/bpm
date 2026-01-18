d04c93a9b1058fa2c1247a6b150d158720bad873
Brian Martiniello <bmartiniello@gmail.com>
2026-01-18 11:30:17 -0500

    develop: Update GraphStub

--- code/core/src/test/GraphStub.cxx
+++ code/core/src/test/GraphStub.cxx
@@ -1,6 +1,88 @@
 
 
 #include <bpm/core/Graph.hxx>
+#include <bpm/core/Logger.hxx>
+
+
+bpm::core::Node::Node(const std::string& name,
+                      bool hasInputPort,
+                      std::size_t& maxLevel,
+                      bool& circularDependency)
+   : name_(name)
+   , level_()
+   , upstreamNodes_()
+   , downstreamNodes_()
+   , maxLevel_(maxLevel)
+   , circularDependency_(circularDependency)
+   , hasInputPort_(hasInputPort)
+{
+}
+
+
+void bpm::core::Node::addUpstreamNode(Node& /* node */)
+{
+}
+
+
+void bpm::core::Node::addDownstreamNode(Node& /* node */)
+{
+}
+
+
+void bpm::core::Node::updateLevel(std::size_t /* newLevel */,
+                                  const std::string& /* originatingNodeName */)
+{
+}
+
+
+void bpm::core::Node::updateLevel(std::size_t /* newLevel */)
+{
+}
+
+
+bool bpm::core::Node::setLevel(std::size_t /* newLevel */)
+{
+   return false;
+}
+
+
+std::string bpm::core::Node::toString(const std::string& leadingText) const
+{
+   auto out = leadingText + "name (" + name_ + ")" +
+              "\n" + leadingText + "level (" + std::to_string(level_) + ")";
+
+   out += "\n" + leadingText + "upstreamNodes.size() (" + std::to_string(upstreamNodes_.size()) + ")";
+   auto index = -1U;
+   for (const auto upstreamNode : upstreamNodes_)
+   {
+      ++index;
+      out += "\n" + leadingText + "upstreamNodes[" + std::to_string(index) + "] (" + upstreamNode->name_ + ")";
+   }
+
+   out += "\n" + leadingText + "downstreamNodes.size() (" + std::to_string(downstreamNodes_.size()) + ")";
+   index = -1U;
+   for (const auto downstreamNode : downstreamNodes_)
+   {
+      ++index;
+      out += "\n" + leadingText + "downstreamNodes[" + std::to_string(index) + "] (" + downstreamNode->name_ + ")";
+   }
+
+   return out;
+}
+
+
+std::string bpm::core::Graph::toString(const std::string& leadingText) const
+{
+   auto out = leadingText + "graph_.size() (" + std::to_string(graph_.size()) + ")";
+   auto index = -1U;
+   for (const auto& pair : graph_)
+   {
+      ++index;
+      const auto prefix = leadingText + "graph_[" + std::to_string(index) + "].";
+      out += "\n" + pair.second.toString(prefix);
+   }
+   return out;
+}
 
 
 bool bpm::core::Graph::addNode(const std::string& /* name */,
@@ -10,14 +92,18 @@ bool bpm::core::Graph::addNode(const std::string& /* name */,
 }
 
 
-std::string bpm::core::Graph::toString(const std::string& /* leadingText */) const
-{
-   return "";
-}
-
-
 bool bpm::core::Graph::connectNodes(const std::string& /* upstreamName */,
                                     const std::string& /* downstreamName */)
 {
    return false;
+}
+
+
+void bpm::core::Graph::listNodesByLevel()
+{
+}
+
+
+void bpm::core::Graph::assignMaxLevel()
+{
 }
