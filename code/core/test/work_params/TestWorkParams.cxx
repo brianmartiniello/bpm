e3010b51b8e2e2b8da400956b85d8c35e4b77c1a
Brian Martiniello <bmartiniello@gmail.com>
2026-01-31 18:05:47 -0500

    develop: Update TestWorkParams

--- code/core/test/work_params/TestWorkParams.cxx
+++ code/core/test/work_params/TestWorkParams.cxx
@@ -9,19 +9,59 @@
 
 #include <bpm/core/Logger.hxx>
 
-struct Unrelated {};
+template<typename T>
+class Data
+{
+   public :
+      Data() = default;
+      ~Data() = default;
+      T* ptr() { return &data_; };
+   private :
+      T data_ = {};
+};
+
+struct ModParams
+{
+   std::size_t scaleFactor_;
+   std::size_t offset_;
+};
+
+class WorkBase
+{
+   public :
+      WorkBase() = default;
+      virtual ~WorkBase() = default;
+      virtual std::string name() = 0;
+      virtual void initialize() = 0;
+      virtual void execute() = 0;
+};
+
+class ModWork : public WorkBase
+{
+   public :
+      ModWork() { BPM_TRACE_COUT(""); };
+      ~ModWork() override { BPM_TRACE_COUT(""); };
+      std::string name() override { return "ModWork"; };
+      void initialize() override { BPM_TRACE_COUT(""); };
+      void execute() override { BPM_TRACE_COUT(""); };
+   private:
+      Data<ModParams> params_ = {};
+};
 
 ///////////////////////////////////////////////
 ///////////////////////////////////////////////
 
-class TestDerivedFrom : public ::testing::Test
+class TestWorkParams : public ::testing::Test
 {
    public:
 
-      TestDerivedFrom() = default;
+      TestWorkParams() = default;
 
       void test()
       {
+         ModWork modWork;
+         modWork.initialize();
+         modWork.execute();
       }
 
    private:
@@ -29,14 +69,9 @@ class TestDerivedFrom : public ::testing::Test
 };
 
 
-TEST_F(TestDerivedFrom, test)
+TEST_F(TestWorkParams, test)
 {
    test();
-}
-
-
-TEST_F(TestDerivedFrom, testT)
-{
 }
 
 
